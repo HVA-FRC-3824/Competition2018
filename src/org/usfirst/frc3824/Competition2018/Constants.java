5be5812164848ca883636bdc08b330954745dc8f
JohnYoung <john.young3210@gmail.com>
2018-01-27 10:26:44 -0500

    Added the elevator and intake subsystems.

--- src/org/usfirst/frc3824/Competition2018/Constants.java
+++ src/org/usfirst/frc3824/Competition2018/Constants.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc3824.Competition2018;
 
-public final class Constants {
-	
+public final class Constants
+{
 	// Chassis constants
-	public static double ChassisEncoderDistancePerPulse = 0.01;	// distance per pulse in FEET
+	public static double ChassisEncoderDistancePerPulse = 0.01; // distance per
+																// pulse in FEET
 
-	
 }
