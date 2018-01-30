636a696b2b2cbac2740889fa540079dde8571d64
JOHNSON-ASUS1\Johnson Family ASUS1 <jlj3322@gmail.com>
2018-01-29 20:26:04 -0500

    Adding Intake subsystem code and functions.

--- src/org/usfirst/frc3824/Competition2018/Constants.java
+++ src/org/usfirst/frc3824/Competition2018/Constants.java
@@ -5,5 +5,11 @@ public final class Constants
 	// Chassis constants
 	public static double ChassisEncoderDistancePerPulse = 0.01; // distance per
 																// pulse in FEET
+	// Intake constants
+	public static int TalonInitialCommunicationTimeout = 10; // timeout during setup (in ms)
 
+	public static double IntakePIDParamF = 0;
+	public static double IntakePIDParamP = 0;
+	public static double IntakePIDParamI = 0;
+	public static double IntakePIDParamD = 0;
 }
